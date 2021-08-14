d89cddcb3fcfd1d1bdaa3756ce9c5e5b5f960572
LinaGamer15 <71581584+LinaGamer15@users.noreply.github.com>
2021-08-14 17:41:41 +0400

    Initial Commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, send_file, abort
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import SubmitField
@@ -8,9 +8,11 @@ import glob
 import cv2
 import pytesseract
 import os
+# create file ignore_file.py and in .git/info/exclude add /ignored_file.py
+from ignored_file import SECRET_KEY, path_to_tesseract
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = SECRET_KEY
 
 
 class ImageUpload(FlaskForm):
@@ -51,14 +53,39 @@ def home():
 @app.route('/<path>')
 def rec_text(path):
     extension = path.split('.')[-1]
-    pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+    pytesseract.pytesseract.tesseract_cmd = path_to_tesseract
     img = cv2.imread('static/images/' + path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     config = r'--oem 3 --psm 6'
     text = pytesseract.image_to_string(img, config=config, lang='rus')
-    texts = text.strip().split('\n\n')
+    i = text.find('\n')
+    new_text = text[:i] + '\n\n' + text[i + 1:]
+    text_l = new_text.split()
+    texts = new_text.strip().split('\n\n')
+    text_to_file = new_text.strip().replace('\n\n', ';').replace('\n', '').replace(';', ';\n')
+    name_file = ''
+    for i1 in range(len(text_l)):
+        if text_l[i1] == 'Уровень':
+            for i2 in range(0, i1):
+                if i2 == i1 - 1:
+                    name_file += f'{text_l[i2]}'
+                else:
+                    name_file += f'{text_l[i2]}_'
+            file = open(f'files/{name_file}.txt', 'w+', encoding='utf-8')
+            file.write(text_to_file)
+            break
     rescale_image = scale_image('static/images/' + path, f'photo.{extension}', 700)
-    return render_template('rec_text.html', texts=texts, path=f'photo.{extension}')
+    if os.path.isfile(f'files/{name_file}.txt'):
+        return render_template('rec_text.html', texts=texts, path=f'photo.{extension}', filename=name_file)
+
+
+@app.route('/send-file/<filename>', methods=['GET', 'POST'])
+def send(filename):
+    try:
+        return send_file(f'files\\{filename}.txt', mimetype='txt', attachment_filename=f'{filename}.txt',
+                         as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
 
 
 if __name__ == '__main__':
